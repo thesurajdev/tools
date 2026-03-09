952a31a3bbf744e106b03f7ef9d78106479c1263
Suraj Kumar <thesurajdev@gmail.com>
2026-03-09 07:11:27 +0000

    Suppress hydration warning on root body

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
+        suppressHydrationWarning
         className={`${inter.variable} font-sans antialiased`}
       >
         {children}
